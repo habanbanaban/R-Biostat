6743c25f31bf3b76d07e90e9d0eae462f03a4ae9
habanbanaban <58214047+habanbanaban@users.noreply.github.com>
2020-09-15 09:23:26 +0200

    Add files via upload

---
+++ Sammanfattning_R.R
@@ -0,0 +1,166 @@
+lab1_dat <- read.csv("C:/Users/User/Documents/KI/Biostatistics/data1.csv",
+                     header = TRUE, sep = ";", stringsAsFactors = TRUE)
+attach(lab1_dat)
+#hur många kvinnor
+sum(sex == "f")
+#hur många människor
+length(sex)
+#vilka är kvinnor? (siffror index)
+which(sex == "f")
+#hur stor andel kvinnor
+sum(sex == "f")/length(sex)
+mean(sex == "f")
+#hur gamla är kvinnorna?
+age[sex == "f"]
+# Compute the minimum and maximum age, separately in males and females.
+tapply(age,sex, range) # tapply(to_what, by_what, what_function)
+#male or female
+length(sex == "f" | sex == "m")
+
+
+#frequentist probability
+
+#when flipping a coin
+coinflip <- sample(c("tail","head"), size=10000, replace=TRUE)
+est_prob <- sum(coinflip=="head")/length(coinflip) #which is the same as
+mean(coinflip == "head")
+est_prob
+
+
+#conditional probability
+mean(smoke == 1)
+mean(smoke == 0)
+mean(gene == "AG")
+mean(gene == "AA")
+mean(gene == "GG")
+#what is the probability that you are a smoker given that you have gene "AA" vs. gene "GG"
+mean(smoke[gene=='AA']=='1') #which is the same as
+tapply(smoke, gene == "AA", mean)
+mean(smoke[gene=="GG"]=="1")
+#How much does the chance of being a smoker change from general population to carrying the gene "AA"
+mean(smoke[gene=='AA']=='1')/mean(gene == "AA")
+
+#relative frequency
+
+barplot(table(age)/length(age), ylab = "probability", xlab = "age")
+
+#binomial distibution
+
+#cdf
+#1- P(X <= n_success - 1)
+#what is the probability that 2 people from the dataset are smokers when randomly selecting 5?
+mean(smoke) #gives the probability of a person being a smoker, 0.5
+dbinom(2, size = 5, prob = 0.5)
+#what is the probability of selecting 2 or fewer smokers out of 5 random picks?
+dbinom(0, size = 5, prob = 0.5)+
+dbinom(1, size = 5, prob = 0.5)+
+dbinom(2, size = 5, prob = 0.5)
+#or
+pbinom(2, size = 5, prob = 0.5)
+#roll 3 dice and let X be the number of 6s rolled. What is the distribution of X?
+dist<- c(dbinom(0, size = 3, prob = 0.167),
+dbinom(1, size = 3, prob = 0.167),
+dbinom(2, size = 3, prob = 0.167),
+dbinom(3, size = 3, prob = 0.167))
+nroll <- c(0,1,2,3)
+plot(dist ~ nroll, xlab = "number of sixes", ylab = "probability")
+min(dist)
+max(dist)
+#roll 2 dice and let X be number of 1s rolled. what is pmf for X?
+dbinom(0, size = 2, prob = 0.167)
+dbinom(1, size = 2, prob = 0.167)
+dbinom(2, size = 2, prob = 0.167)
+
+#poisson distribution
+
+###If there are twelve cars crossing a bridge per minute on average,
+###find the probability of having seventeen or more cars crossing the bridge in a particular minute.
+1 -ppois(16, lambda = 12)
+#or
+ppois(16, lambda = 12, lower = FALSE)
+#what is the probability of 13 cars passing?
+dpois(13, lambda = 12)
+
+#Continuous uniform distribution
+
+#Draw 100 random numbers between 1 and 3
+runif(100, min = 1, max = 3)
+barplot(runif(100, min = 1, max = 3))
+
+#normal distribution
+
+#what percentage of the participants are older than 60?
+mean(age)
+sd(age)
+pnorm(60, mean = 56, sd = 11.23487, lower.tail = FALSE)
+#check to see if corr with
+mean(age > 60)
+# Calculate the probability that a normal random variable with mean 10
+# and standard deviation 4 is between -2 and 2 or between 14 and 18.
+# That is, the probability of the union of the events -2 < X < 2 and 14 < X < 18.
+pnorm(2, mean = 10, sd = 4) - pnorm(-2, mean = 10, sd = 4) +
+  pnorm(18, mean = 10, sd = 4) - pnorm(14, mean = 10, sd = 4)
+
+#plotting
+
+#histogram, 1 variable, frequency  
+hist(age)
+#Histogram of age of females (association of age with sex)
+hist(age[sex == "f"]) #which is the same as
+tapply(age, sex == "f",hist)
+#barplot, 1 variable,age of each person in set
+barplot(age)
+#barplot of table, nr. of persons that have each age in set
+barplot(table(age))
+# Cut age into four equally sized intervals and do a barplot of the
+# proportion of smokers in the 4 age groups
+age_cat <- cut(age, 4)
+barplot(tapply(smoke, age_cat, mean),
+        xlab = "age",
+        ylab = "smokers")
+
+#Represent graphically the association between age and smoking.
+# For example, you can do a barplot of proportion of patients who smoke in each age group
+barplot(tapply(smoke, age, mean),
+        ylab = "smoke",
+        xlab = "age")
+#boxplot, plots summary
+summary(age)
+boxplot(age)
+#multiple variable boxplots
+boxplot(age ~ sex)
+#uses the data of a table consisting of
+table(sex,age)
+#scatterplot, 2 variables
+plot(age,gene)
+plot(age, gene, col = c("orange", "darkgreen","blue")[gene]) # plot them all
+legend("topleft", legend = c("AA", "AG","AC"), fill = c("orange", "darkgreen","blue"))
+
+#Functions
+
+# confidence interval: confidence(%) that mu is in interval
+t.test(age, conf.level = 0.95)
+t.test(age, conf.level = 0.99)
+t.test(age, conf.level = 1)
+qnorm(0.025, lower = F)
+
+#99% confidence interval without using t.test function
+mean(lab4_dat$y1) + c(-1, 1) * qt(0.005, df = nrow(lab4_dat) - 1, lower = F) *
+  sd(lab4_dat$y1) / sqrt(nrow(lab4_dat))
+
+#models
+
+#calculating using normally distributed models
+var(sample)
+mean(sample)
+
+detach(lab1_dat)
+rm(x)
+rm(y)
+rm(age_cat)
+rm(b1cat)
+rm(b2cat)
+rm(coinflip)
+rm(nroll)
+rm(dist)
+rm(est_prob)
